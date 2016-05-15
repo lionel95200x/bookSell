91f58c350738abbf857671f0b286452bf7fe44f0
lionel95200x <lionel95200@gmail.com>
2016-05-15 15:07:34 +0200

    Test unitaire Karma

--- karma.conf.js
+++ karma.conf.js
@@ -30,8 +30,7 @@ module.exports = function(config) {
 
     // list of files to exclude
     exclude: [
-      'app/scripts/ngCart/src/ngCart_test.js',
-      'bower_components/ngCart/src/ngCart_test.js'
+      
     ],
 
 
